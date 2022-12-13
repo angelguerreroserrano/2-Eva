3f7ca70746ddf2febc8b5846bb4459c0d40edc04
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2022-12-13 10:21:34 +0100

    Add files via upload

---
+++ 2. Entrada de Datos.cpp
@@ -0,0 +1,13 @@
+//Programa entrada de datos.
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x,y,z;
+	cout<<"Introduce tres numeros: "<<endl; cin>>x>>y>>z;
+	cout<<"Has introducido tres variables: ";
+	cout<<x<<", "<<y<<", "<<z<<endl;
+	return 0;
+}
