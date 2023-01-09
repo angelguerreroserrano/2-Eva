b9a494670355fec98a318006ec463405e7c479c0
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-01-09 10:07:08 +0100

    Add files via upload

---
+++ 7. For.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int cont;
+	
+	for(cont=1;cont<=4;cont++){	//Desde 1, hasta 4, ascendente (++)
+		cout<<"Estoy contento "<<cont<<endl;
+	}
+	
+	return 0;
+}
