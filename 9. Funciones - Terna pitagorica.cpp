2eabf49e7d6424464383dc35d25e72f05fcb78ea
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-01-13 10:00:10 +0100

    Update 9. Funciones - Terna pitagorica.cpp

--- 9. Funciones - Terna pitagorica.cpp
+++ 9. Funciones - Terna pitagorica.cpp
@@ -1,26 +1,25 @@
 #include<iostream>
 
-
 using namespace std;
 
 int espitagorica(int num1,int num2,int num3){
 	//Decidimos cual es la hipotenusa
 	int c1,c2,hip;
-	int respuesta;
+	int respuesta=0;
 	if (num1>num2 && num1>num3){
-		hip=num1
-		c1=num2
-		c2=num3
+		hip=num1;
+		c1=num2;
+		c2=num3;
 	}
 	else if (num2>num1 && num2>num3){
-		hip=num2
-		c1=num1
-		c2=num3
+		hip=num2;
+		c1=num1;
+		c2=num3;
 	}
 	else{
-		hip=num3
-		c1=num1
-		c2=num2
+		hip=num3;
+		c1=num1;
+		c2=num2;
 	}
 	if (hip*hip==c1*c1+c2*c2){
 		respuesta=1;
@@ -37,11 +36,11 @@ int main(){
 	cout<<"Segundo numero: "; cin>>num2;
 	cout<<"Tercer numero: "; cin>>num3;
 	respuesta=espitagorica(num1,num2,num3);
-	if (respuesta=0){
-		cout<<num1<<", "<<num2<<" y "<<num3<<"NO son una terna pitagorica.";
+	if (respuesta==0){
+		cout<<num1<<", "<<num2<<" y "<<num3<<" NO son una terna pitagorica.";
 	}
 	else{
-		cout<<num1<<", "<<num2<<" y "<<num3<<"SI es una terna pitagorica.";
+		cout<<num1<<", "<<num2<<" y "<<num3<<" SI es una terna pitagorica.";
 	}
 	return 0;
 }
