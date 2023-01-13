86969b6a86c3eaea1b6cab224eba0e3ee3f3a331
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-01-13 10:26:03 +0100

    Rename 10. Antiguo C++.cpp to 10. Antiguo C++ - Dirección de una variable.cpp

--- 10. Antiguo C++ - Dirección de una variable.cpp
+++ 10. Antiguo C++ - Dirección de una variable.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<stdio.h>	//Librería que nos permite utilizar algunas funciones del antiguo C++
+#include<stdio.h>	//LibrerÃ­a que nos permite utilizar algunas funciones del antiguo C++
 using namespace std;
 
 int main(){
@@ -7,9 +7,9 @@ int main(){
 	int x;
 	int *px;	//De esta manera guardas x y px. Pero px tendra los valores que tenga x
 	x=3;
-	px=&x;	//& equivale a la dirección de x
+	px=&x;	//& equivale a la direcciÃ³n de x
 	printf("x=%d\n",x);
-	printf("px=%x\n,",px);	//Aquí ponemos %x porque queremos poner la dirección de x
+	printf("px=%x\n,",px);	//AquÃ­ ponemos %x porque queremos poner la direcciÃ³n de x
 	printf("nuemro1=%d, numero2=%f",x,y);	//Funciona como un cout. %d es un entero, %f en un decimal (float)
 	return 0;
 }
