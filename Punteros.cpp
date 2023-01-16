70439baa29d796d2ffe4096b504c4a25157d9ce6
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-01-16 10:23:19 +0100

    Add files via upload

---
+++ Punteros.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main(){
+	char letra[]="historia";
+	int i;
+	for(i=0;i<=7;i++){
+		printf("Dame la letra[%d]=%c\n",i,letra[i]);
+	}
+	return 0;
+}
