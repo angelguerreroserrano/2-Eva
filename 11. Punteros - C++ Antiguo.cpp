ddf88db54585eee5c6a7bade897d824a35ecabfb
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-01-16 10:33:49 +0100

    Update 11. Punteros - C++ Antiguo.cpp

--- 11. Punteros - C++ Antiguo.cpp
+++ 11. Punteros - C++ Antiguo.cpp
@@ -7,7 +7,7 @@ int main(){
 	char letra[]="historia";
 	int i;
 	for(i=0;i<=7;i++){
-		printf("Dame la letra[%d]=%c\n",i,letra[i]);
+		printf("Dame la letra[%d]=%c en la posicion=%x\n",i,letra[i],&letra[i]);
 	}
 	return 0;
 }
