5fc68b566b61a11db80aae5d9435b5ebc21fe256
Ángel Guerrero Serrano <113890447+angelguerreroserrano@users.noreply.github.com>
2023-02-21 15:44:48 +0100

    Update 16. Voluntario.cpp

--- 16. Voluntario.cpp
+++ 16. Voluntario.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main(){
-	int lista[10], lista_ord[10], lista_mayor[3];
-	int cont,i,j;
+	int lista[10], lista_ord[10], lista_mayor[3], lista_par[10];
+	int cont,i,j,n_pares=0;
 	int aux;
+	int suma,media;
 	
 	//Primero pedimos los 10 numeros y los vamos guardando en lista
 	for(cont=0;cont<10;cont++){
@@ -18,25 +19,29 @@ int main(){
 		cout<<lista[cont]<<" ";
 	}
 	
-	//Creamos la variable lista_ord para ordenar la lista ahí
-	lista_ord[10]=lista[10];
+	//Creamos la variable lista_ord para ordenar la lista ahi.
+	//Lo tenemos que hacer de numero en numero
+	for(cont=0;cont<10;cont++){
+		lista_ord[cont]=lista[cont];
+	}
 	//Ordenamos la lista
 	for(i=0;i<9;i++){
 		for(j=0;j<9;j++){
-			if(lista[j]<lista[j+1]){
-				aux=lista[j];
-				lista[j]=lista[j+1];
-				lista[j+1]=aux;
+			if(lista_ord[j]<lista_ord[j+1]){
+				aux=lista_ord[j];
+				lista_ord[j]=lista_ord[j+1];
+				lista_ord[j+1]=aux;
 			}
 		}
 	}
 	//Mostramos la lista
 	cout<<"\nLa lista ordenada es: ";
 	for(cont=0;cont<10;cont++){
-		cout<<lista[cont]<<" ";
+		cout<<lista_ord[cont]<<" ";
 	}
 	
-	//La variable lista_mayor contiene los tres numeros mayores. Que los sacamos de los tres primeros numeros de la listas ordenada.
+	//La variable lista_mayor contiene los tres numeros mayores.
+	//Que los sacamos de los tres primeros numeros de la listas ordenada.
 	for(cont=0;cont<3;cont++){
 		lista_mayor[cont]=lista_ord[cont];
 	}
@@ -45,5 +50,24 @@ int main(){
 	for(cont=0;cont<3;cont++){
 		cout<<lista_mayor[cont]<<" ";
 	}
+	
+	//Creamos la variable lista_par para ir aÃ±adiendo los numeros pares de uno en uno y ordenados
+	for(cont=0;cont<10;cont++){
+		if((lista_ord[cont]%2)==0){
+			lista_par[n_pares]=lista_ord[cont];
+			n_pares++;
+		}
+	}
+	cout<<"\nLos tres primeros pares son: ";
+	for(cont=0;cont<3;cont++){
+		cout<<lista_par[cont]<<" ";
+	}
+	
+	//Media de los tres valores mayores
+	for(cont=0;cont<3;cont++){
+		suma=suma+lista_ord[cont];
+	}
+	media=(suma/3);
+	cout<<"\nLa media de los tres valores mayores es de: "<<media;
 	return 0;
 }
